9bbeaf075ad9a3a0f9830163ca6bb1c408dbe713
CookieSilent <benf3632@gmail.com>
2019-11-07 17:39:30 +0200

    Change login design

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:wave/config.dart';
 import 'sign_in.dart';
 import 'main_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wave/wave.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -48,85 +50,140 @@ class _LoginPageState extends State<LoginPage> {
         var screenWidth = MediaQuery.of(context).size.width;
         var screenHeight = MediaQuery.of(context).size.height;
         return Scaffold(
-                body: Stack(
-                    children: <Widget> [
-                        ClipPath(
-                            child: Container(
-                                decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                        begin: Alignment.topLeft,
-                                        end: Alignment.bottomRight,
-                                        stops: [0.1, 0.3, 0.4, 0.5],
-                                        colors: [
-                                            Colors.orange[300],
-                                            Colors.orange[400],
-                                            Colors.orange[600],
-                                            Colors.orange[900],
-                                        ]
-                                    ),
-                                ),
-                                width: screenWidth,
-                                height: 300,
+          resizeToAvoidBottomInset: false,
+          body: Stack(
+              children: <Widget> [
+                Container(
+                    child: Text('Login', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
+                    margin: EdgeInsets.only(top: 250, left: screenWidth / 2 - 55),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(topRight: Radius.circular(50.0), bottomRight: Radius.circular(50.0)),
+                        border: Border.all(color: Colors.grey),
+                    ),
+                    child: Column(                    
+                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget> [
+                          Theme(
+                            data: Theme.of(context).copyWith(splashColor: Colors.transparent),
+                            child: TextField(                                               
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,                                    
+                                  prefixIcon: Icon(Icons.person),
+                                  hintText: 'Username',
+                              )
                             ),
-                            clipper: BottomWaveClipper(),
-                        ),
-                        Container(
-                            child: Text('Login', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold)),
-                            margin: EdgeInsets.only(top: 250, left: screenWidth / 2 - 55),
-                        ),
-                        Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(topRight: Radius.circular(50.0), bottomRight: Radius.circular(50.0)),
-                                border: Border.all(color: Colors.grey),
+                          ), 
+                          Divider(),
+                          Theme(
+                            data: Theme.of(context).copyWith(splashColor: Colors.transparent),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(Icons.lock),
+                                  hintText: 'Password',
+                              )
                             ),
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: <Widget> [
-                                    TextField(
-                                        decoration: InputDecoration(
-                                            border: InputBorder.none,
-                                            hintText: 'Username',
-                                        )
-                                    ), 
-                                    Divider(),
-                                    TextField(
-                                        obscureText: true,
-                                        decoration: InputDecoration(
-                                            border: InputBorder.none,
-                                            hintText: 'Password',
-                                        )
-                                    ),
-                                ]
-                            ),
-                            width: 250,
-                            height: 105,
-                            margin: EdgeInsets.only(top: 360),
+                          ),
+                      ]
+                    ),
+                    width: 250,
+                    height: 120,
+                    margin: EdgeInsets.only(top: 360),
+                ),
+                Container(
+                    child: _signInButton(),
+                    margin: EdgeInsets.only(top: 390, left: screenWidth - 80),
+                ),
+                Container(
+                    child: FloatingActionButton(
+                      heroTag: "Login",
+                        child: Icon(Icons.arrow_forward),
+                        backgroundColor: Colors.greenAccent,
+                        onPressed: () {}
+                    ),
+                    margin: EdgeInsets.only(top: 390, left: screenWidth - 150)
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                  Container(
+                      height: 200,
+                      child: RotationTransition(
+                        turns: AlwaysStoppedAnimation(180 / 360),
+                        child: WaveWidget(
+                          config: CustomConfig(
+                            gradients: [
+                              [Colors.red, Color(0xEEF44336)],
+                              [Colors.red[800], Color(0x77E57373)],
+                              [Colors.orange, Color(0x66FF9800)],
+                              [Colors.yellow, Color(0x55FFEB3B)]
+                            ],
+                            durations: [35000, 19440, 10800, 6000],
+                            heightPercentages: [0.20, 0.23, 0.25, 0.30],
+                            blur: MaskFilter.blur(BlurStyle.solid, 10),
+                            gradientBegin: Alignment.bottomLeft,
+                            gradientEnd: Alignment.topRight,
+                          ),
+                          waveAmplitude: 0,
+                          size: Size(
+                            double.infinity,
+                            double.infinity,
+                          ),
                         ),
-                        Container(
-                            child: _signInButton(),
-                            margin: EdgeInsets.only(top: screenHeight - screenHeight / 3, left: screenWidth / 2 + 50),
-                        )
-                    ]
-                )
+                      )
+                    ),
+                    Container(
+                    height: 200,
+                    margin: EdgeInsets.only(top:screenHeight - 400),
+                    child: WaveWidget(
+                      config: CustomConfig(
+                        gradients: [
+                          
+                          [Colors.blue, Color(0xFF12EBC5)],
+                          [Colors.blue, Colors.blue[500]],
+                          [Colors.blueAccent, Colors.yellow],
+                          [Colors.blue, Colors.green],
+                        ],
+                        durations: [35000, 19440, 10800, 6000],
+                        heightPercentages: [0.50, 0.23, 0.25, 0.30],
+                        blur: MaskFilter.blur(BlurStyle.solid, 10),
+                        gradientBegin: Alignment.bottomLeft,
+                        gradientEnd: Alignment.topRight,
+                      ),
+                      waveAmplitude: 0,
+                      size: Size(
+                        double.infinity,
+                        double.infinity,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ]
+          )
         );
     }
 
     Widget _signInButton() {
-        return OutlineButton(
+        return FloatingActionButton(
+            heroTag: "GoogleLogin",
             splashColor: Colors.grey,
+            backgroundColor: Colors.white,
             onPressed: () {
-                signInWithGoogle().then((user) {print('Finished sign in');
+                signInWithGoogle().then((user) {
+                  if (user != null) {
+                    print('Finished sign in');
                     Navigator.pushReplacement(context,
-                            MaterialPageRoute(
-                                builder: (context) => MainPage(user: user)
-                            )
+                      MaterialPageRoute(
+                        builder: (context) => MainPage(user: user)
+                      )
                     );
+                  }
                 }); 
             },
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-            highlightElevation: 0,
-            borderSide: BorderSide(color: Colors.grey),
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
             child: Image(image: AssetImage("assets/google_logo.png"), height: 35),
         );
     }
@@ -165,13 +222,14 @@ class Dialogs {
 }
 
 
-class BottomWaveClipper extends CustomClipper<Path> {
+class TopWaveClipper extends CustomClipper<Path> {
     @override
     Path getClip(Size size) {
         var path = Path();
         path.lineTo(0.0, size.height);
-        path.quadraticBezierTo(size.width / 2 - 50, size.height - 59, (size.width / 2 ) - 50, size.height / 2 - 30);
-        path.quadraticBezierTo(size.width / 2 - 30, 0.0, size.width - 50, 0.0);
+
+        path.quadraticBezierTo(size.width / 2 - 60, size.height - 59, (size.width / 2 ) - 50, size.height / 2 - 30);
+        path.quadraticBezierTo(size.width / 2 - 50, 0.0, size.width - 50, 0.0);
         
         path.close();
         return path;
@@ -180,3 +238,30 @@ class BottomWaveClipper extends CustomClipper<Path> {
     @override
     bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
+
+
+class BottomWaveCliper extends CustomClipper<Path> {
+    @override
+    Path getClip(Size size) {
+        var path = Path();
+        path.moveTo(size.width, 0);
+
+        path.lineTo(0, size.height);
+        path.lineTo(size.width, size.height);
+        path.lineTo(size.width, 0);
+        
+
+        path.quadraticBezierTo(size.width / 2 + 60, 10, size.width / 2, size.height / 2);
+        path.quadraticBezierTo(size.width / 2 - 40, size.height - 20, 0, size.height);
+
+        path.close();
+        return path;
+    }
+
+    @override
+    bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+
+
+
--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -6,45 +6,47 @@ final GoogleSignIn googleSignIn = GoogleSignIn();
 
 Future<FirebaseUser> signInWithGoogle() async {
     final GoogleSignInAccount  googleSignInAccount = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication = 
-            await googleSignInAccount.authentication;
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication = 
+              await googleSignInAccount.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-        accessToken: googleSignInAuthentication.accessToken,
-        idToken: googleSignInAuthentication.idToken,
-    );
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+      );
 
-    await _auth.signInWithCredential(credential);
+      await _auth.signInWithCredential(credential);
 
 
-    final FirebaseUser currentUser = await _auth.currentUser();
-    
-    
-    return currentUser;
+      final FirebaseUser currentUser = await _auth.currentUser();
+      
+      
+      return currentUser;
+    }
+    return null;
 }
 
 void signOutGoogle() async {
     await googleSignIn.signOut();
-
     print('User Sign Out');
 }
 
 Future<FirebaseUser> signSilentGoogle() async {
    final GoogleSignInAccount googleAcc = await googleSignIn.signInSilently();
    if (googleAcc != null) {
-        final GoogleSignInAuthentication googleSignInAuthentication = 
-            await googleAcc.authentication;
-        
-        final AuthCredential credential = GoogleAuthProvider.getCredential(
-            accessToken: googleSignInAuthentication.accessToken,
-            idToken: googleSignInAuthentication.idToken,
-        );
+      final GoogleSignInAuthentication googleSignInAuthentication = 
+          await googleAcc.authentication;
+      
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+      );
 
-        await _auth.signInWithCredential(credential);
+      await _auth.signInWithCredential(credential);
 
-        final FirebaseUser currentUser = await _auth.currentUser();
+      final FirebaseUser currentUser = await _auth.currentUser();
 
-        return currentUser;
+      return currentUser;
 
    }
    return null;
